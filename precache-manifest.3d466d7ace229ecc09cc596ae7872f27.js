5b2fbb2f7c4fb055ffa98a3c6b88b8bc37a818de
FrancoCalcina <63270775+FrancoCalcina@users.noreply.github.com>
2020-06-08 22:40:32 -0500

    3

---
+++ precache-manifest.3d466d7ace229ecc09cc596ae7872f27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88a3002085e887caa5c01ced28aa99c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faef6eacb3148335caf4",
+    "url": "/static/js/2.c9db042a.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.c9db042a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1149e9c6029526654dc8",
+    "url": "/static/js/main.27149c69.chunk.js"
+  },
+  {
+    "revision": "ef03fad86a919ef26f97",
+    "url": "/static/js/runtime-main.3ada473c.js"
+  }
+]);
